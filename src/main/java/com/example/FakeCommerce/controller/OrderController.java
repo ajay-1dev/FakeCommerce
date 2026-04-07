b210e8a9a993a7dec0a0aae348aad7945b96004c
ajay-1dev <ajay.developerr@gmail.com>
2026-04-07 19:56:12 +0530

    changes made in Order controller

--- src/main/java/com/example/FakeCommerce/controller/OrderController.java
+++ src/main/java/com/example/FakeCommerce/controller/OrderController.java
@@ -4,20 +4,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.FakeCommerce.dtos.CreateOrderRequestDTO;
+import com.example.FakeCommerce.dtos.GetOrderResponseDTO;
 import com.example.FakeCommerce.dtos.OrderDto;
+import com.example.FakeCommerce.dtos.UpdateOrderRequestDTO;
 import com.example.FakeCommerce.schema.Order;
 import com.example.FakeCommerce.services.OrderService;
+import com.example.FakeCommerce.utils.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -40,8 +47,14 @@ public class OrderController {
     
 
     @PostMapping()
-    public void createOrder(@RequestBody CreateOrderRequestDTO createOrderRequestDTO) {        
-        orderService.createOrder(createOrderRequestDTO);
+    public GetOrderResponseDTO createOrder(@RequestBody CreateOrderRequestDTO createOrderRequestDTO) {        
+        return orderService.createOrder(createOrderRequestDTO);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ApiResponse<GetOrderResponseDTO>> updateOrder(@PathVariable("id") Long id , @RequestBody UpdateOrderRequestDTO updateOrderRequestDTO){
+        GetOrderResponseDTO response = orderService.updateOrder(id, updateOrderRequestDTO);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(ApiResponse.success(response,"Sucessfully updated"));
     }
 
     @DeleteMapping("{id}")
